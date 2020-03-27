cc3ec22446707d93677f276d01f0a4912a1594fa
Maximilian Eber <maximilianeber@gmail.com>
2020-03-27 14:08:54 +0100

    Refactor model

--- policy/seir.py
+++ policy/seir.py
@@ -1,118 +1,129 @@
-def simulator(parameters, start, periods, dT):
+import datetime
+from datetime import timedelta
+import pandas as pd
 
-    """Modified SEIR model"""
 
-    # Retrieving parameters
+class Seir(object):
+    def __init__(self, params, start, dT=0.01):
+        self.params = params
+        self.start = start
+        self.dT = dT
 
-    # List with integers which denote days when r_0 changes, e.g. [25, 55, ...]
-    r_0_days = parameters["r_0_days"]
-    # List with floats for the r_0 values associated with these changes,
-    # e.g. [3.5, 2.5, ...]
-    r_0_values = parameters["r_0_values"]
+    def simulate(self, r0_steps):
+        # Convert policy steps to daily paths
+        self.policy_path = self._steps_to_path(r0_steps, dT=self.dT)
 
-    t_infectious = parameters["t_infectious"]
-    t_incubation = parameters["t_incubation"]
-    t_recovery_mild = parameters["t_recovery_mild"]
-    t_recovery_severe = parameters["t_recovery_severe"]
-    t_hospital_lag = parameters["t_hospital_lag"]
-    t_death = parameters["t_death"]
-    p_severe = parameters["p_severe"]
-    p_fatal = parameters["p_fatal"]
+        # Initialize results with starting values
+        self.results = {k: [v] for k, v in self.start.items()}
+        self.results["T"] = [self.policy_path[0][0]]  # first date in policy path
 
-    a = 1 / t_incubation
-    gamma = 1 / t_infectious
+        for time, r0 in self.policy_path:
+            self.iterate(time=time, r0=r0)
 
-    p_mild = 1 - p_severe - p_fatal
+    def iterate(self, time, r0):
+        """Iterate current state forward by one step with current r0"""
+        # Roll forward time
+        time = time + timedelta(days=self.dT)
 
-    niter = int(periods / dT)
+        # Retrieve parameters
+        t_infectious = self.params["t_infectious"]
+        t_incubation = self.params["t_incubation"]
+        t_recovery_mild = self.params["t_recovery_mild"]
+        t_recovery_severe = self.params["t_recovery_severe"]
+        t_hospital_lag = self.params["t_hospital_lag"]
+        t_death = self.params["t_death"]
+        p_severe = self.params["p_severe"]
+        p_fatal = self.params["p_fatal"]
 
-    series = {
-        "T": [start["T"]],
-        "S": [start["S"]],
-        "E": [start["E"]],
-        "I": [start["I"]],
-        "I_mild": [start["I_mild"]],
-        "I_severe_home": [start["I_severe_home"]],
-        "I_severe_hospital": [start["I_severe_hospital"]],
-        "I_fatal_home": [start["I_fatal_home"]],
-        "I_fatal_hospital": [start["I_fatal_hospital"]],
-        "R_from_mild": [start["R_from_mild"]],
-        "R_from_severe": [start["R_from_severe"]],
-        "Dead": [start["Dead"]],
-    }
+        p_mild = 1 - p_severe - p_fatal
+        a = 1 / t_incubation
+        gamma = 1 / t_infectious
+        beta = r0 * gamma  # current beta given r0 in policy path
 
-    # Creation of the r_0 path (a list of length niter / a step function of r_0 values)
-    r_0_path = []
-    previous_day = 0
-    for cc in range(len(r_0_days)):
+        # Basic SEI(R) dynamics
+        S = self.results["S"][-1]
+        E = self.results["E"][-1]
+        I = self.results["I"][-1]  # infectious
 
-        r_0_path += (r_0_days[cc] - previous_day) * [r_0_values[cc]]
-        previous_day = r_0_days[cc]
+        dS = (-beta * I * S) * self.dT
+        dE = (beta * I * S - a * E) * self.dT
+        dI = (a * E - gamma * I) * self.dT
 
-    # Iterations
-    for i in range(niter):
+        self.results["S"].append(S + dS)
+        self.results["E"].append(E + dE)
+        self.results["I"].append(I + dI)
 
-        # Computing the current beta
-        beta = r_0_path[int(i * dT)] / t_infectious
+        # Addititonal clinical dynamics:
+        # - Infections turn into mild cases, severe cases, and fatal cases
+        # - All three cases initially start at home
+        # - Mild cases directly recover from home
+        # - Severe case become hospitalized and recover
+        # - Fatal cases become hospitalized and die
+        I_mild = self.results["I_mild"][-1]
+        I_severe_home = self.results["I_severe_home"][-1]
+        I_severe_hospital = self.results["I_severe_hospital"][-1]
+        I_fatal_home = self.results["I_fatal_home"][-1]
+        I_fatal_hospital = self.results["I_fatal_hospital"][-1]
+        R_from_mild = self.results["R_from_mild"][-1]
+        R_from_severe = self.results["R_from_severe"][-1]
+        Dead = self.results["Dead"][-1]
 
-        # Current model iteration
+        # Case 1/3: Mild
+        dI_mild = (p_mild * gamma * I - (1 / t_recovery_mild) * I_mild) * self.dT
 
-        T = series["T"][-1]
-        S = series["S"][-1]
-        E = series["E"][-1]
-        I = series["I"][-1]
-        I_mild = series["I_mild"][-1]
-        I_severe_home = series["I_severe_home"][-1]
-        I_severe_hospital = series["I_severe_hospital"][-1]
-        I_fatal_home = series["I_fatal_home"][-1]
-        I_fatal_hospital = series["I_fatal_hospital"][-1]
-        R_from_mild = series["R_from_mild"][-1]
-        R_from_severe = series["R_from_severe"][-1]
-        Dead = series["Dead"][-1]
-
-        dS = (-beta * I * S) * dT
-        dE = (beta * I * S - a * E) * dT
-        dI = (a * E - gamma * I) * dT
-
-        # Flows into three mutually exclusive courses of the illness
-        # A: Mild course
-        dI_mild = (p_mild * gamma * I - (1 / t_recovery_mild) * I_mild) * dT
-
-        # B: Severe course (two steps)
+        # Case 2/3: Severe
         dI_severe_home = (
             p_severe * gamma * I - (1 / t_hospital_lag) * I_severe_home
-        ) * dT
+        ) * self.dT
         dI_severe_hospital = (
             (1 / t_hospital_lag) * I_severe_home
             - (1 / t_recovery_severe) * I_severe_hospital
-        ) * dT
+        ) * self.dT
 
-        # C: Fatal course (two steps; in this version, fatal cases follow the same
-        # route as severe cases: first home, then hospital. This tries to ensure that
-        # they show up in the hospital statistics of the model as well)
-        dI_fatal_home = (p_fatal * gamma * I - (1 / t_hospital_lag) * I_fatal_home) * dT
+        # Case 3/3: Severe
+        dI_fatal_home = (
+            p_fatal * gamma * I - (1 / t_hospital_lag) * I_fatal_home
+        ) * self.dT
         dI_fatal_hospital = (
             (1 / t_hospital_lag) * I_fatal_home - (1 / t_death) * I_fatal_hospital
-        ) * dT
+        ) * self.dT
 
-        # Final flows from courses of ilness into recovery or death
-        dR_from_mild = ((1 / t_recovery_mild) * I_mild) * dT
-        dR_from_severe = ((1 / t_recovery_severe) * I_severe_hospital) * dT
-        dDead = ((1 / t_death) * I_fatal_hospital) * dT
+        # Final states
+        dR_from_mild = ((1 / t_recovery_mild) * I_mild) * self.dT
+        dR_from_severe = ((1 / t_recovery_severe) * I_severe_hospital) * self.dT
+        dDead = ((1 / t_death) * I_fatal_hospital) * self.dT
 
-        # Storing simulated time series
+        # Store simulated time series
+        self.results["T"].append(time)
+        self.results["I_mild"].append(I_mild + dI_mild)
+        self.results["I_severe_home"].append(I_severe_home + dI_severe_home)
+        self.results["I_severe_hospital"].append(I_severe_hospital + dI_severe_hospital)
+        self.results["I_fatal_home"].append(I_fatal_home + dI_fatal_home)
+        self.results["I_fatal_hospital"].append(I_fatal_hospital + dI_fatal_hospital)
+        self.results["R_from_mild"].append(R_from_mild + dR_from_mild)
+        self.results["R_from_severe"].append(R_from_severe + dR_from_severe)
+        self.results["Dead"].append(Dead + dDead)
 
-        series["T"].append(T + dT)
-        series["S"].append(S + dS)
-        series["E"].append(E + dE)
-        series["I"].append(I + dI)
-        series["I_mild"].append(I_mild + dI_mild)
-        series["I_severe_home"].append(I_severe_home + dI_severe_home)
-        series["I_severe_hospital"].append(I_severe_hospital + dI_severe_hospital)
-        series["I_fatal_home"].append(I_fatal_home + dI_fatal_home)
-        series["I_fatal_hospital"].append(I_fatal_hospital + dI_fatal_hospital)
-        series["R_from_mild"].append(R_from_mild + dR_from_mild)
-        series["R_from_severe"].append(R_from_severe + dR_from_severe)
-        series["Dead"].append(Dead + dDead)
+    @property
+    def data(self, resampling_rule="1d"):
+        df = pd.DataFrame.from_dict(self.results).set_index("T")
+        df = df.resample(resampling_rule).first()
+        df["Hospitalised"] = (
+            df["I_severe_hospital"] + df["I_fatal_hospital"]
+        )
+        return df
 
-    return series
+    @staticmethod
+    def _steps_to_path(r0_steps, dT=1):
+        """Convert dictionary of R0 steps to R0 path and associated dates"""
+        dates = [datetime.datetime.fromisoformat(d) for d, _ in r0_steps]
+        r0s = [r0 for _, r0 in r0_steps]
+        date_path = []
+        r0_path = []
+        for i, x in enumerate(dates[:-1]):
+            length = int((dates[i + 1] - dates[i]).days / dT)
+            dates_regime = [dates[i] + timedelta(days=dT * n) for n in range(length)]
+            r0_path_regime = [r0s[i]] * length
+            date_path.extend(dates_regime)
+            r0_path.extend(r0_path_regime)
+        return list(zip(date_path, r0_path))
